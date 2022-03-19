3fae18804a62ff9edb27bfc368e084da37ff485a
DongQing <3327613384@qq.com>
2022-03-19 18:08:54 +0800

    no message

---
+++ transformer/swish.py
@@ -0,0 +1,10 @@
+"""Swish() activation function for Conformer."""
+
+import torch
+
+
+class Swish(torch.nn.Module):
+    """Construct an Swish object."""
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Return Swish activation function."""
+        return x * torch.sigmoid(x)
